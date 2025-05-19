d71f051e6f5b9d80af650e6e1fbfe91b10abd6b3
Konstantin <konstantin.klein+github@hochfrequenz.de>
2025-05-19 16:32:27 +0200

    wip

--- WannaBeeClient/WannaBeeClient.IntegrationTest/ValidationTests.cs
+++ WannaBeeClient/WannaBeeClient.IntegrationTest/ValidationTests.cs
@@ -53,6 +53,29 @@ RFF+Z19:DE69617355664H850VEVXMWUXGZ1R0MKP'
 UNT+20+ALEXANDE846768'
 UNZ+1+ALEXANDE121116'
 """;
+
+    private const string Invalid55042 = // Wrong MeLo and MaLo ID
+"""
+UNB+UNOC:3+9910902000001:500+9900269000000:500+241204:0617+ALEXANDE121116'
+UNH+ALEXANDE846768+UTILMD:D:11A:UN:S1.1'
+BGM+E01+ALEXANDE846768BGM'
+DTM+137:202412040617?+00:303'
+NAD+MS+9910902000001::293'
+NAD+MR+9900269000000::293'
+IDE+24+ALEXANDE307337741'
+DTM+76:202412162300?+00:303'
+STS+7++ZE3'
+AGR+9:Z04'
+LOC+Z16+DASISTKEINEMALO'
+LOC+Z17+DASISTKEINEMELOID'
+RFF+Z13:55042'
+NAD+Z07+++Muster:Max:::Herr:Z01'
+NAD+Z08+++Muster:Max:::Herr:Z01+Am Ihmeufer::201+Hannover++30149+DE'
+NAD+Z05+++Muster:Max:::Herr:Z01+Am Ihmeufer::201+Hannover++30149+DE'
+RFF+Z19:DE69617355664H850VEVXMWUXGZ1R0MKP'
+UNT+20+ALEXANDE846768'
+UNZ+1+ALEXANDE121116'
+""";
     private readonly ClientFixture _client;
 
     private readonly IWannaBeeAuthenticator _authenticator;
@@ -81,16 +104,21 @@ UNZ+1+ALEXANDE121116'
     }
 
     [Theory]
-    [InlineData(true)]
-    [InlineData(false)]
-    public async Task Errornous_Validations_Work_As_Expected(bool withBoneyCombPaths)
+    [InlineData(true, Invalid44042)]
+    [InlineData(false, Invalid44042)]
+    [InlineData(true, Invalid55042)]
+    [InlineData(false, Invalid55042)]
+    public async Task Errornous_Validations_Work_As_Expected(
+        bool withBoneyCombPaths,
+        string invalidEdifact
+    )
     {
         var httpClientFactory = _client.HttpClientFactory;
         IEdifactAhbValidator client = new WannaBeeRestClient(httpClientFactory, _authenticator);
 
         var actual = await client.Validate(
             new ValidateEdifactRequest(
-                Edifact: Invalid44042,
+                Edifact: invalidEdifact,
                 IncludeBoneycombPaths: withBoneyCombPaths
             )
         );
